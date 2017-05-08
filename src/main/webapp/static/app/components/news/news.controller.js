8ff85c55356d9f047e4e152da36ede5bebb2ebfd
Milana Jaksic Ujfalusi <milana986@gmail.com>
2017-05-08 15:49:32 +0200

    creating a new category page

--- src/main/webapp/static/app/components/news/news.controller.js
+++ src/main/webapp/static/app/components/news/news.controller.js
@@ -11,6 +11,29 @@
 
     $scope.news = news;
 
+<<<<<<< Updated upstream
+=======
+    // $scope.filter = {
+    //   "category": $stateParams.category ? $stateParams.category : ''
+    //   //search: $scope.search
+    // }
+    //
+    // $scope.$watch("filter", function() {
+    //   getNewsByCategory();
+    // }, true);
+    //
+    // var getNewsByCategory = function() {
+    //   NewsService.getAllnews({
+    //     "filter": {
+    //       "category": $scope.filter.category
+    //     }
+    //   }).then(function(data) {
+    //     $scope.news = data;
+    //   });
+    // };
+    // getNewsByCategory();
+
+>>>>>>> Stashed changes
     $scope.addNews = function() {
       $state.go("main.addNews");
     };
