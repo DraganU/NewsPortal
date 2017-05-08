4ec087685e5f7a6818efe7797ba4ab6d6e263d7a
Milana Jaksic Ujfalusi <milana986@gmail.com>
2017-05-08 16:11:31 +0200

    Merge branch 'master' into category-page-create
    # Conflicts:
    #	src/main/webapp/static/app/components/news/news.controller.js

--- src/main/webapp/static/app/components/news/news.controller.js
+++ src/main/webapp/static/app/components/news/news.controller.js
@@ -11,25 +11,25 @@
 
     $scope.news = news;
 
-    $scope.filter = {
-      category: $stateParams.category ? $stateParams.category : ''
-      //search: $scope.search
-    }
-
-    $scope.$watch("filter", function() {
-      getNewsByCategory();
-    }, true);
-
-    var getNewsByCategory = function() {
-      NewsService.getAllnews({
-        "filter": {
-          "category": $scope.filter.category
-        }
-      }).then(function(data) {
-        $scope.news = data;
-      });
-    };
-    getNewsByCategory();
+    // $scope.filter = {
+    //   category: $stateParams.category ? $stateParams.category : ''
+    //   //search: $scope.search
+    // }
+    //
+    // $scope.$watch("filter", function() {
+    //   getNewsByCategory();
+    // }, true);
+    //
+    // var getNewsByCategory = function() {
+    //   NewsService.getAllnews({
+    //     "filter": {
+    //       "category": $scope.filter.category
+    //     }
+    //   }).then(function(data) {
+    //     $scope.news = data;
+    //   });
+    // };
+    // getNewsByCategory();
 
     $scope.addNews = function() {
       $state.go("main.addNews");
